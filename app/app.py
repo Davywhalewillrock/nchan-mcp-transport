cb95aae41264cbb270490c9affef87fc8b244c89
lloydzhou <lloydzhou@qq.com>
2025-03-12 23:46:51 +0800

    update

--- app/app.py
+++ app/app.py
@@ -64,6 +64,11 @@ async def create_openapi_mcp_server():
     logger.debug("Server1 started %r", server1.router.routes)
     app.include_router(server1.router)
 
+    url = "https://ghfast.top/https://raw.githubusercontent.com/APIs-guru/openapi-directory/refs/heads/main/APIs/notion.com/1.0.0/openapi.yaml"
+    server2 = await OpenAPIMCP.from_openapi(url, publish_server="http://nchan:80")
+    logger.error("Server1 started %r", server2.router.routes)
+    app.include_router(server2.router)
+
 asyncio.run(create_openapi_mcp_server())
 
 if __name__ == "__main__":
